dcaae9e438fa8d67344889cc6fb1a114798c4a36
Yoann Chaudet <yoannchaudet@github.com>
2022-09-26 16:31:37 -0700

    add

--- .github/flows/hello.js
+++ .github/flows/hello.js
@@ -5,3 +5,9 @@ const config = {
 flow.onInit('main').do(async () => {
   console.log('Hello from a first flow')
 })
+
+flow.onIssue()
+  .withEvent('opened')
+  .withLabel('hello').do(async() => {
+    console.log(`hello on context ${JSON.stringify(github)}`)
+  })
